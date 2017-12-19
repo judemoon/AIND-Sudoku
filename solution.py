048938d8a31be9d20bfdca78ea685859be74adcc
Jude Moon <4judemoon@gmail.com>
2017-12-19 15:34:35 -0500

    Add solution codes

--- solution.py
+++ solution.py
@@ -14,7 +14,7 @@ units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
 peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
 
 
-def naked_twins(values):
+def naked_twins(dictionary):
     """Eliminate values using the naked twins strategy.
 
     Parameters
@@ -42,10 +42,34 @@ def naked_twins(values):
     strategy repeatedly).
     """
     # TODO: Implement this function!
-    raise NotImplementedError
+    # find all boxes with two values
+    for unit in unitlist:
+        pairs = [box for box in unit if len(dictionary[box]) == 2]
+        if len(pairs) > 1:
+            twin_dict = {} # pair_value as a key, list of pair as a value
+            for pair in pairs:
+                pair_value = dictionary[pair]
+                if pair_value in twin_dict:
+                    twin_dict[pair_value].append(pair)
+                else:
+                    twin_dict[pair_value] = [pair]
+            # remove key with the number of pairs != 2
+            naked_twins = {k: v for k, v in twin_dict.items() if len(v) == 2}
+            # if there is naked twins, get all values of naked twins
+            if naked_twins:
+                naked_twins_values = ''
+                for v in naked_twins:
+                    naked_twins_values = naked_twins_values + v
+                # eliminate the values of the naked twins from non-twins values
+                for box in unit:
+                    if len(dictionary[box])>2:
+                        for digit in dictionary[box]:
+                            if digit in naked_twins_values:
+                                dictionary[box] = dictionary[box].replace(digit,'')
+    return dictionary
 
 
-def eliminate(values):
+def eliminate(dictionary):
     """Apply the eliminate strategy to a Sudoku puzzle
 
     The eliminate strategy says that if a box has a value assigned, then none
@@ -62,10 +86,15 @@ def eliminate(values):
         The values dictionary with the assigned values eliminated from peers
     """
     # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+    solved_values = [box for box in dictionary.keys() if len(dictionary[box]) == 1]
+    for box in solved_values:
+        digit = dictionary[box]
+        for peer in peers[box]:
+            dictionary[peer] = dictionary[peer].replace(digit,'')
+    return dictionary
 
 
-def only_choice(values):
+def only_choice(dictionary):
     """Apply the only choice strategy to a Sudoku puzzle
 
     The only choice strategy says that if only one box in a unit allows a certain
@@ -86,10 +115,15 @@ def only_choice(values):
     You should be able to complete this function by copying your code from the classroom
     """
     # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+    for unit in unitlist:
+        for digit in '123456789':
+            dplaces = [box for box in unit if digit in dictionary[box]]
+            if len(dplaces) == 1:
+                dictionary[dplaces[0]] = digit
+    return dictionary
 
 
-def reduce_puzzle(values):
+def reduce_puzzle(dictionary):
     """Reduce a Sudoku puzzle by repeatedly applying all constraint strategies
 
     Parameters
@@ -104,10 +138,25 @@ def reduce_puzzle(values):
         no longer produces any changes, or False if the puzzle is unsolvable 
     """
     # TODO: Copy your code from the classroom and modify it to complete this function
-    raise NotImplementedError
+    stalled = False
+    while not stalled:
+        # Check how many boxes have a determined value
+        solved_values_before = len([box for box in dictionary.keys() if len(dictionary[box]) == 1])
+        # Use the Eliminate Strategy
+        dictionary = eliminate(dictionary)
+        # Use the Only Choice Strategy
+        dictionary = only_choice(dictionary)
+        # Check how many boxes have a determined value, to compare
+        solved_values_after = len([box for box in dictionary.keys() if len(dictionary[box]) == 1])
+        # If no new values were added, stop the loop.
+        stalled = solved_values_before == solved_values_after
+        # Sanity check, return False if there is a box with zero available values:
+        if len([box for box in dictionary.keys() if len(dictionary[box]) == 0]):
+            return False
+    return dictionary
 
 
-def search(values):
+def search(dictionary):
     """Apply depth first search to solve Sudoku puzzles in order to solve puzzles
     that cannot be solved by repeated reduction alone.
 
@@ -127,7 +176,25 @@ def search(values):
     and extending it to call the naked twins strategy.
     """
     # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+    
+    # First, Iterate eliminate() and only_choice() using the previous procedure
+    dictionary = iterate(dictionary)
+    if dictionary is False:
+        return False ## Failed earlier
+    if all(len(dictionary[s]) == 1 for s in boxes): 
+        return dictionary ## Solved!
+    
+    # Choose one of the unfilled squares with the fewest possibilities
+    n,s = min((len(dictionary[s]), s) for s in boxes if len(dictionary[s]) > 1)
+    
+    # Now use recurrence to solve each one of the resulting sudokus, 
+    # and if one returns a value (not False), return that answer!
+    for value in dictionary[s]:
+        new_sudoku = dictionary.copy()
+        new_sudoku[s] = value
+        attempt = search(new_sudoku)
+        if attempt:
+            return attempt
 
 
 def solve(grid):
